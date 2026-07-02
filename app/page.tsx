127d6e86f1fc0cf8989c3e4003b220863acea34d
John Becker De Castro <tmacataotao@gmail.com>
2026-07-03 02:04:02 +0800

    Added background ideo and changed fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,30 @@
 export default function Home() {
   return (
-    <main style={{
-      height: "100vh",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  textAlign: "center",
-  padding: "24px",
-  fontFamily: "Bessita Handwriting Free, sans-serif",
-  background: "linear-gradient(to bottom, #ffffff, #e5e7eb)",
-  color: "#111827"
-    }}>
-      <h1 style={{ fontSize: "3rem", marginBottom: "20px" }}>
-        Hope In Each Moment
-      </h1>
+    <main
+  className="min-h-screen flex flex-col items-center justify-center text-center px-6  text-white-900">
+    
+{/* Background Video */}
+  <video className= "absolute top-0 left-0 w-full h-full object-cover -z-10" autoPlay loop muted playsInline>
+    <source src="/images/Fiber Optics Resized.mp4" type="video/mp4"/> 
+  </video>
 
-      <p style={{ fontSize: "1.3rem", maxWidth: "600px", marginBottom: "20px" }}>
-        God is preparing something for you here.
+
+{/* Foreground Content */}
+<h1 className="text-8xl mb-25 font-['allura'] max-w-4xl stroke-[5px] stroke-black text-white drop-shadow-lg">
+  Hope In Each Moment
+</h1>
+
+      <p className="text-4xl max-w-4xl mb-20 font-['ScriptC'] [word-spacing:0.5rem]" stroke-1px stroke-black text-white="true">
+  God is preparing something for you here.
+
       </p>
 
-      <p style={{ maxWidth: "600px", opacity: 0.75 }}>
+      <p className="max-w-2xl mb-5 opacity-75">
         We are prayerfully building a place where you can find Scripture,
         prayer, worship, encouragement, and guidance—one moment at a time.
       </p>
 
-      <p style={{ marginTop: "40px", fontSize: "0.9rem", opacity: 0.5 }}>
+      <p className="mt-10 text-sm mb-20 opacity-50">
         Project Nehemiah • Under Construction
       </p>
     </main>

7d24ad97c36d911c832548ec3960f038c8e3a6f7
John Becker De Castro <tmacataotao@gmail.com>
2026-07-04 01:30:32 +0800

    updated global fonts, fixed font color and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,54 @@
+import localfont from "next/font/local";
+
+const bessita = localfont({ src: "../public/fonts/Bessita Handwriting Free.otf" });
+const brightwall = localfont({ src: "../public/fonts/Brightwall Personal Use Only.ttf" });
+const geometr212 = localfont({ src: "../public/fonts/Geometr212 BkCn BT Book.ttf" });
+const jasper = localfont({ src: "../public/fonts/JaspersHandwriting-Regular.ttf" });
+const techb = localfont({ src: "../public/fonts/techb___.ttf" });
+
+
 export default function Home() {
   return (
-    <main
-  className="min-h-screen flex flex-col items-center justify-center text-center px-6  text-white-900">
+  <main
+  className="relative min-h-screen flex flex-col items-center justify-center text-center px-6 text-white">
     
-{/* Background Video */}
-  <video className= "absolute top-0 left-0 w-full h-full object-cover -z-10" autoPlay loop muted playsInline>
-    <source src="/images/Fiber Optics Resized.mp4" type="video/mp4"/> 
-  </video>
+ {/* Background Video Container */}
+  <div className="absolute inset-0 z-0">
+    <video
+      className="w-full h-full object-cover"
+      autoPlay
+      loop
+      muted
+      playsInline
+    >
+      <source src="/images/Fiber Optics Resized.mp4" type="video/mp4" />
+    </video>
+  </div>
 
+  {/* Overlay */}
+  <div className="absolute inset-0 bg-black/60 z-10"></div>
 
-{/* Foreground Content */}
-<h1 className="text-8xl mb-25 font-['allura'] max-w-4xl stroke-[5px] stroke-black text-white drop-shadow-lg">
+  {/* Content */}
+  <div className="relative z-20">
+    <h1 className={`${bessita.className} text-7xl mb-30`}>
   Hope In Each Moment
 </h1>
 
-      <p className="text-4xl max-w-4xl mb-20 font-['ScriptC'] [word-spacing:0.5rem]" stroke-1px stroke-black text-white="true">
-  God is preparing something for you here.
+    <p className={`${jasper.className} text-4xl mb-16`}>
+      God is preparing something for you here.
+    </p>
 
-      </p>
+    <p className="max-w-2xl mt-20 mb-5 opacity-75 mx-auto">
+      We are prayerfully building a place where you can find Scripture,
+      prayer, worship, encouragement, and guidance — one moment at a time.
+    </p>
 
-      <p className="max-w-2xl mb-5 opacity-75">
-        We are prayerfully building a place where you can find Scripture,
-        prayer, worship, encouragement, and guidance—one moment at a time.
-      </p>
+    <p className={`${techb.className} mt-10 text-sm opacity-50`}>
+      Project Nehemiah • Under Construction
+    </p>
 
-      <p className="mt-10 text-sm mb-20 opacity-50">
-        Project Nehemiah • Under Construction
-      </p>
-    </main>
+    </div>
+
+  </main>
   );
-}
+} 

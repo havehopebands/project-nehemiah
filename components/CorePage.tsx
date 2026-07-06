2199eb30dc3ca7a6df9b5e2afe7056cad705b74a
John Becker De Castro <tmacataotao@gmail.com>
2026-07-06 23:43:41 +0800

    Add core page background and shared fonts

--- components/CorePage.tsx
+++ components/CorePage.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { brightwall, jasper } from "@/lib/fonts";
 
 type CorePageProps = {
   title: string;
@@ -7,24 +8,41 @@ type CorePageProps = {
 
 export default function CorePage({ title, subtitle }: CorePageProps) {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-center text-center px-6 bg-black text-white">
+  
+  <main className="relative min-h-screen flex flex-col items-center justify-center text-center px-6 text-white overflow-hidden">
 
-  <Image
-    src="/icon.png"
-    alt="Hope In Each Moment Logo"
-    width={120}
-    height={120}
-    className="mb-8"
-  />
+    {/* Background Image */}
+    <Image
+      src="/images/construction-bg.jpg"
+      alt="Under construction background"
+      fill
+      className="object-cover -z-20"
+    />
 
-  <h1 className="text-5xl mb-6">
-    {title}
-  </h1>
+    {/* Dark Overlay */}
+    <div className="absolute inset-0 bg-black/60 -z-10"></div>
 
-  <p className="text-xl opacity-75">
-    {subtitle}
-  </p>
+    {/* Content */}
+    <div>
+      <Image
+        src="/icon.png"
+        alt="Hope In Each Moment Logo"
+        width={120}
+        height={120}
+        priority
+        className="mb-8 mx-auto"
+      />
 
-</main>
-  );
+      <h1 className={`${brightwall.className} text-5xl mb-6`}>
+        {title}
+      </h1>
+
+      <p className="text-xl opacity-75">
+        {subtitle}
+      </p>
+    </div>
+
+  </main>
+);
+  
 }
---
+++ lib/fonts.ts
@@ -0,0 +1,21 @@
+import localFont from "next/font/local";
+
+export const bessita = localFont({
+  src: "../public/fonts/Bessita Handwriting Free.otf",
+});
+
+export const brightwall = localFont({
+  src: "../public/fonts/Brightwall Personal Use Only.ttf",
+});
+
+export const geometr212 = localFont({
+  src: "../public/fonts/Geometr212 BkCn BT Book.ttf",
+});
+
+export const jasper = localFont({
+  src: "../public/fonts/JaspersHandwriting-Regular.ttf",
+});
+
+export const techb = localFont({
+  src: "../public/fonts/techb___.ttf",
+});

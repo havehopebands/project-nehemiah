70b9cd35dd27c383b159cbab61d142329875ed93
John Becker De Castro <tmacataotao@gmail.com>
2026-08-14 02:55:57 +0800

    added og image

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log(
-  "SUPABASE SECRET KEY EXISTS:",
-  !!process.env.SUPABASE_SECRET_KEY
-);
+
 
 export const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
